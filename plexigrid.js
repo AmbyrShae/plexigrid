af6a95a3a45cac26b4c5f8ae303a12a6aa9fc2ca
Ambyr-Shae Jarrell <arjarrel@uno.edu>
2016-09-21 21:01:50 -0500

    Almost Done
    All that is left to do is add a Rules box that will always
    remain visible.
    Add popovers explaining rules while highlighting boxes for
    tutorial.
    Add numbered hints to columns and rows.

--- plexigrid.js
+++ plexigrid.js
@@ -1,12 +1,22 @@
 $( document ).ready(function() {
 
-  var btnPress = "mark";
+  var btnPress = "mark",
+      funcArray = [],
+      selectedBoxes,
+      markedBox,
+      markedBoxes,
+      markedPass,
+      unmarkedBoxes,
+      unmarkedPass,
+      success;
 
   tutorial();
+  funcArray.push(levelOne, levelTwo);
 
   // click events
   $(".box").click(clickedBox);
-  $("button").click(changeColor);
+  $(".colorBtn").click(changeColor);
+  $(".levelBtn").click(clearGrid);
 
   // square clicked
   function clickedBox(box) {
@@ -24,13 +34,19 @@ $( document ).ready(function() {
     gridComplete();
   }
 
+// changes what color is used depending on what button is clicked
   function changeColor(btn) {
+
     btnPress = btn.target.innerHTML;
+
   }
 
+// initialize tutorial
   function tutorial() {
-    var selectedBoxes = [11, 15, 33, 41, 45, 51, 52, 53, 54, 55],
-        markedBox;
+
+    document.querySelector(".level>span").innerHTML = "tutorial";
+
+    selectedBoxes = [11, 15, 33, 41, 45, 51, 52, 53, 54, 55];
 
     for(var i = 0; i < selectedBoxes.length; i++) {
       markedBox = document.getElementById(selectedBoxes[i]);
@@ -39,25 +55,82 @@ $( document ).ready(function() {
 
   }
 
-  function gridComplete() {
-    var markedBoxes = document.querySelectorAll('[data-marked="true"]'),
-        unmarkedBoxes = document.querySelectorAll('[data-marked="false"]'),
-        success,
-        box,
-        boxMarked,
-        boxClass;
+// initialize tutorial
+  function levelOne() {
 
-    success = Array.prototype.every.call(markedBoxes, function(el) {
-      return (el.className.includes("red") == true);
-    });
-    // success = [markedBoxes[0], markedBoxes[1]].every(checkClass);
+    document.querySelector(".level>span").innerHTML = "level 1";
 
-    console.log(success);
+    selectedBoxes = [11, 12, 14, 15, 21, 24, 32, 33, 34, 42, 45, 51, 52, 54, 55];
+
+    for(var i = 0; i < selectedBoxes.length; i++) {
+      markedBox = document.getElementById(selectedBoxes[i]);
+      markedBox.setAttribute("data-marked", "true");
+    }
+
   }
 
-  function checkClass(box) {
-    console.log(box);
-    return true;
+// initialize tutorial
+  function levelTwo() {
+
+    document.querySelector(".level>span").innerHTML = "level 2";
+
+    selectedBoxes = [14, 15, 22, 23, 24, 41, 42, 43, 44, 45, 53];
+
+    for(var i = 0; i < selectedBoxes.length; i++) {
+      markedBox = document.getElementById(selectedBoxes[i]);
+      markedBox.setAttribute("data-marked", "true");
+    }
+
+  }
+// checks to see that puzzle is complete
+// if it is complete, display success and enable level button
+  function gridComplete() {
+
+    markedBoxes = document.querySelectorAll('[data-marked="true"]'),
+    unmarkedBoxes = document.querySelectorAll('[data-marked="false"]'),
+    success = false;
+
+    markedPass = Array.prototype.every.call(markedBoxes, function(el) {
+      return (el.className.includes("red") == true);
+    });
+
+    unmarkedPass = Array.prototype.every.call(unmarkedBoxes, function(el) {
+      return (el.className.includes("red") == false);
+    });
+
+    if(markedPass && unmarkedPass) {
+      success = true;
+      document.querySelector(".complete").style.visibility="visible";
+      document.querySelector(".levelBtn").disabled= false;
+    }
+
+    if(success == false) {
+      document.querySelector(".complete").style.visibility="hidden";
+      document.querySelector(".levelBtn").disabled= true;
+    }
+
+  }
+
+// clears grid for next level and disables next level button
+  function clearGrid() {
+
+    selectedBoxes = document.querySelectorAll(".box");
+    Array.prototype.forEach.call(selectedBoxes, function(el) {
+      $(el).removeClass("red grey");
+      el.setAttribute("data-marked", "false");
+    });
+
+    document.querySelector(".level>span").innerHTML = "";
+    document.querySelector(".complete").style.visibility="hidden";
+    document.querySelector(".levelBtn").disabled= true;
+
+    if(funcArray.length > 0){
+      funcArray.shift()();
+    }
+    else{
+      document.querySelector(".level>span").innerHTML = "end of game";
+    }
+
   }
 
 });
